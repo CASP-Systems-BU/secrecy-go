026a4f74537eee0a1d2506543db3fa814b106fb5
mfaisal97 <mfaisal@bu.edu>
2024-02-19 20:27:06 +0000

    Added default cors.

--- api/service.go
+++ api/service.go
@@ -3,6 +3,7 @@ package api
 import (
 	"github.com/CASP-Systems-BU/secrecy-go/api/controllers"
 	"github.com/gin-gonic/gin"
+	cors "github.com/rs/cors/wrapper/gin"
 )
 
 // Service is a struct that contains the router.
@@ -19,6 +20,14 @@ func NewService() *Service {
 
 // InitializeRoutes is a function that initializes the routes for the service.
 func (s *Service) InitializeRoutes() {
+
+	s.Router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Origin", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization", "accept", "origin", "Cache-Control", "X-Requested-With"},
+		AllowCredentials: true,
+	}))
+
 	s.Router.GET("/ping", controllers.PingGet)
 	s.Router.POST("/ping", controllers.PingPost)
 }
